2daab4b033f3296f272bcfa508c9f78113f79869
Pratik Shinde <pratikshinde1497@gmail.com>
2020-09-19 18:32:09 +0530

    add restaurant --get single, update, delete

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const Restaurant = require("../models/Restraurant");
 
 const router = express.Router();
@@ -18,6 +18,48 @@ router
     res.status(201).json({
       success: true,
       data: restaurant,
+    });
+  });
+
+router
+  .route("/:id")
+  .get(async (req, res, next) => {
+    const restaurant = await Restaurant.findById(req.params.id);
+    if (!restaurant) {
+      return next(new Error("no restaurant found"));
+    }
+    res.status(200).json({
+      success: true,
+      data: restaurant,
+    });
+  })
+  .put(async (req, res, next) => {
+    const restaurant = await Restaurant.findById(req.params.id);
+    if (!restaurant) {
+      return next(new Error("no restaurant found"));
+    }
+    const updated = await Restaurant.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidation: true,
+      }
+    );
+    res.status(200).json({
+      success: true,
+      data: updated,
+    });
+  })
+  .delete(async (req, res, next) => {
+    const restaurant = await Restaurant.findById(req.params.id);
+    if (!restaurant) {
+      return next(new Error("no restaurant found"));
+    }
+    await Restaurant.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      success: true,
+      data: {},
     });
   });
 
