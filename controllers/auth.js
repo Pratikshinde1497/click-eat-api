43f5ad965944afedd8410e7b6d50ee70b28de5aa
Pratik Shinde <pratikshinde1497@gmail.com>
2020-11-30 11:14:51 +0530

    fix options error

--- controllers/auth.js
+++ controllers/auth.js
@@ -130,9 +130,9 @@ const sendTokenResponse = (user, statusCode, res) => {
   //   expires: new Date( Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
   //   httpOnly: true
   // }
-  if(process.env.NODE_ENV === 'production') {
-    options.secure = true
-  }
+  // if(process.env.NODE_ENV === 'production') {
+  //   options.secure = true
+  // }
   //  send response
   res
     .status(statusCode)
